ce1764588ac5855c14f8ab31071ad7671e53cbda
caleb <kalejaiyecaleb@gmail.com>
2024-10-04 03:57:22 +0100

    commit

--- src/sections/Updates.jsx
+++ src/sections/Updates.jsx
@@ -4,14 +4,14 @@ import { Link } from "react-router-dom";
 
 const Updates = () => {
   return (
-    <div className="md:flex md:flex-row justify-center text-left mx-[50px] md:gap-[100px] gap-8 mt-[100px] flex flex-col items-center md:items-start">
+    <div className="md:flex md:flex-row justify-center text-left mx-[50px] md:gap-[70px] gap-8 mt-[100px] flex flex-col items-center md:items-start">
         <div className="flex flex-col md:items-start items-center text-center md:text-left">
         <h1 className="w-[550px] sm:text-[50px] font-bold text-[35px] leading-[50px] sm:leading-normal">News and Market Updates</h1>
         <p className="md:w-[250px] text-[16px] leading-[25px] text-gray-500 pt-[15px] w-full">Stay Ahead of the curve, get the latest news, trends, and insights from the cryptocurrency world. Be the first to know about market shifts, new regulations, and emerging coins.</p>
         </div>
         <div className="flex flex-col md:gap-3 gap-6">
             {News.map((item, index) => (
-                <div className="flex flex-col bg-gray-200 rounded-md p-[15px] md:w-[400px] w-full cursor-pointer md:h-[130px] justify-between h-[150px] hover:text-white hover:bg-red-700 transition duration-500" key={index}>
+                <div className="flex flex-col bg-gray-200 rounded-md p-[15px] md:w-[450px] w-full cursor-pointer md:h-[130px] justify-between h-[150px] hover:text-white hover:bg-red-700 transition duration-500" key={index}>
                     <Link to={"/blogs"}>
                     <div className="flex text-left items-center">
                         <p className="font-bold text-[13px] text-red-900">{item.category}</p>
